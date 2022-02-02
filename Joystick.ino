64d0f0c99fa07f8a56d3b7d97272636c0a425482
Angel <90753298+ANGEY33@users.noreply.github.com>
2022-02-02 11:38:25 +0100

    Add files via upload

--- Joystick.ino
+++ Joystick.ino
@@ -1,23 +1,48 @@
-
 const int pinBoton = 3; 
 const int pinEjeY = A1; 
 const int pinEjeX = A0; 
+int valorActualEjeX;
+int valorActualEjeY;
+int valorAnteriorEjeX;
+int valorAnteriorEjeY;
+bool encenderLed;
+int valorDeMovimiento = 10;
  
 void setup() {
   pinMode(pinBoton, INPUT);
   digitalWrite(pinBoton, HIGH);
+  pinMode(8,OUTPUT);
   Serial.begin(9600);
 }
- 
+
 void loop() {
+  
+  valorActualEjeX = analogRead(pinEjeX);
   Serial.print("Boton:  ");
   Serial.print(digitalRead(pinBoton));
   Serial.print("\n");
   Serial.print("Eje X: ");
-  Serial.print(analogRead(pinEjeX));
+  Serial.print(valorActualEjeX);
+  Serial.print(valorAnteriorEjeX);
   Serial.print("\n"); //esto es un salto de linea
   Serial.print("Eje Y: ");
   Serial.println(analogRead(pinEjeY));
   Serial.print("\n\n");
   delay(150);
+  if ((valorActualEjeX - valorAnteriorEjeX > valorDeMovimiento) || (valorActualEjeY - valorAnteriorEjeY > valorDeMovimiento)){
+    encenderLed = true; 
+  }
+  else {
+    encenderLed = false;
+  }
+
+  
+  if (encenderLed) {
+    digitalWrite(8, HIGH);
+    Serial.println("Encendiendo Led");
+  }
+  else {
+    digitalWrite(8, LOW);
+  }
+  valorAnteriorEjeX=valorActualEjeX;
 }
